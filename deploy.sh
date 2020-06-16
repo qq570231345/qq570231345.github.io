a5294ef528fed479aafd7ff4246d6a9b3466a731
think <570231347@qq.com>
2020-06-17 00:16:25 +0800

    添加blog

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.zhangyunchen.cc' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你想要部署到 https://<USERNAME>.github.io
+git remote add origin git@github.com:qq570231345/qq570231345.github.io.git
+git push -u origin master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
+# git push -f git@github.com:qq570231345/vuepressBlog.git master:gh-pages
+
+cd -
